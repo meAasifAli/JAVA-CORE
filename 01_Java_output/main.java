c13aabc2447acef18380b50b27c23a616ae3c755
meAasifAli <asif15310@gmail.com>
2025-04-02 20:46:26 +0530

    feat:learned comments in java

--- 01_Java_output/main.java
+++ 01_Java_output/main.java
@@ -1,1 +1,26 @@
 
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); //prints text in a new line
+
+        // we can add more than one println() statements
+        System.out.println("hey");
+        System.out.println("heyo");
+
+        System.out.print("Hello World!"); //prints text in the same line
+        System.out.print("hey");
+        System.out.print("heyo"
+        );
+
+        //escape sequeance
+        System.out.println("Hello \"world\"!");
+
+        //print numbers
+        System.out.println(1);
+        System.out.println(2);
+
+        // we can  also perform operations on numbers in println() or print() methods
+        System.out.println(1 + 2);
+    }
+}
